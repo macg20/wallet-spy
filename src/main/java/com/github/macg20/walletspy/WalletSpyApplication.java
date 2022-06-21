0ab1926c624513a8aeaeeb17b1cd0b4224699f19
macg20 <macgryczka20@gmail.com>
2022-06-21 23:24:23 +0200

    initial project architecture

---
+++ src/main/java/com/github/macg20/walletspy/WalletSpyApplication.java
@@ -0,0 +1,13 @@
+package com.github.macg20.walletspy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletSpyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletSpyApplication.class, args);
+    }
+
+}
